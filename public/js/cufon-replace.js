af07ca98e48505c6a9753e69e29af63c811fac3e
Taras <my_email@example.com>
2014-03-04 23:57:56 +0200

    added font for cufon

--- public/js/cufon-replace.js
+++ public/js/cufon-replace.js
@@ -1,2 +1,2 @@
-Cufon.replace('#menu a, h2, .button1, h3, #ContactForm a', { fontFamily: 'Amaranth', hover:true });
-
+//Cufon.replace('#menu a, h2, .button1, h3, #ContactForm a', { fontFamily: 'Amaranth', hover:true });
+Cufon.replace('#menu a, h2, .button1, h3, #ContactForm a', { fontFamily: 'AvantGardeCTT', hover:true });
